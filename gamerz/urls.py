6680bcc98431cf94936c6cefa0bfdd356e9be235
gonzaloven <gonzaloven@live.com.ar>
2020-08-14 18:35:59 -0300

    Agrego vista para el modelo Producto

--- gamerz/urls.py
+++ gamerz/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from producto.views import ProductoListView, producto_list_view
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^productos/$', ProductoListView.as_view()),
+    url(r'^productos-fbv/$', producto_list_view),
+
 ]
--- producto/views.py
+++ producto/views.py
@@ -1,6 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from .models import Producto
+from django.views.generic import ListView
 from django.shortcuts import render
 
+
 # Create your views here.
+class ProductoListView(ListView):
+    queryset = Producto.objects.all()
+    template_name = "productos/lista.html"
+
+
+def producto_list_view(request):
+    queryset = Producto.objects.all()
+    context = {
+        'lista_de_productos': querySet
+    }
+    return render(request, "productos/lista.html", context)
